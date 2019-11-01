6fa3d871e30b919afaa95a9e2f1ae8e66bc8cb48
Jatinokov <Enkhtaivan1206@gmail.com>
2019-10-31 23:31:47 -0700

    add style and make buttons functional for displaying imaged

--- js/app.js
+++ js/app.js
@@ -1,125 +1,91 @@
-
-// $(document).ready(function(){
-
-//     let api_key = 'QZDK12gEoyQSqJjGcUf0zhRnzG6Mqgaw';
-
-//     // https://api.giphy.com/v1/gifs/search?api_key=ub2GyIwPubylVrPGcrfyAjIyiRSpkmbp&q=&limit=25&offset=0&rating=G&lang=en
-
-//     // дээрх orginal url 
-//     // доорх code ruu convert 
-//     // let url = 'https://api.giphy.com/v1/gifs/search?api_key=' + api_key + '&q=&limit=25&offset=0&rating=G&lang=en';
-
-//     // console.log(url);
-
-//     // function for displaying the giphy data
-//     function renderButtons(){
-//         $('#Container').empty();
-
-//     };
-
-//     $('#searchBTN').on('click', function(event){
-
-//         event.preventDefault();
-
-//     });
-
-
-
-
-//     $('#searchBTN').on('click', function(){
-//         const searchText = $('#Search').val();
-//         const queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + api_key + '&q='+ searchText +'&limit=2&offset=0&rating=G&lang=en';
-
-//         console.log(queryUrl);
-        
-//         $.ajax({
-
-//             type: 'GET',
-//             url: queryUrl
-
-//         }).then(function(response){
-
-//             const data = response.data;
-//             // console.log(response);
-
-//             data.forEach(function(element){
-
-//                 // console.log(data);
-
-//             // $('#Container').append('<img src='+ $(element.images.fixed_height.url)+'>');
-
-//             // console.log(element.images.fixed_height.url);
-//             $('#Container').append('<img src=' + element.images.fixed_height.url+ '/>');
-
-//             });
-//         });
-//     });
-// });
-
-
 $(document).ready(function(){
 
-    var giphy = ['dog', 'cat','bird'];
+var giphies = ['Mr. Joker', 'Mr.Bean', 'Mr.JimCerry'];
+    // console.log(giphies);
 
-    // Generic function for capturing the movie name from the data-attribute
-
-    function displayGif(){
-        var giphyName = $(this).attr('data-name');
-
-        var api_key = 'ub2GyIwPubylVrPGcrfyAjIyiRSpkmbp';
-
-        // var queryUrl = 'http://api.giphy.com/v1/gifs/search?q=dog&api_key='+api_key;
-        // console.log(giphyName);
-        var queryUrl = "http://api.giphy.com/v1/gifs/search?t=" + giphyName + "&api_key="+ api_key +"&limit=5";
-        console.log(queryUrl);
-
-        $.ajax({
-            type: ' GET',
-            url: queryUrl
-
-        }).then(function(response){
-
-            var imageUrl = response;
-            console.log(response);
-            // $('#container').append();
-
-        });
-    }
 
     function renderButtons(){
 
-        $('#container').empty();
+        $('#button-View').empty();
+        for(var i = 0; i < giphies.length; i++){
+                
+            $('#button-View').append('<button class="btn">' + giphies[i] + '</button>');
+        //  console.log(giphies[i]);  
 
-    for(var i=0; i < giphy.length; i++){
+        };
+    
+    };
+    renderButtons();
+        
 
-        var btn = $('<button>');
+        // This function handles events where one button is clicked
 
-        btn.addClass('giphy');
-        btn.attr('data-name', giphy[i]);
-        btn.text(giphy[i]);
-
-        $('#container').append(btn);
-    }
- }
-    $('#searchBTN').on('click', function(event){
+    $('#add-Giphy').on('click', function(event){
         event.preventDefault();
 
-        var gif = $('#search').val().trim();
+    var giphy = $('#giphy-Input').val();
 
-        giphy.push(gif);
+    $('#button-View').append('<button class="btn">' + giphy + '</button>');
+        // console.log(giphy);
 
-        renderButtons();
 
     });
-    console.log(giphy);
 
-    $(document).on('click', '.giphy', displayGif);
-    //$('.giphy').on('click', alertthem);
+    renderButtons();
 
- renderButtons();
-   
+    // $('.btn').on('click', function(){
+    
+    //     // console.log("i'm mr coffee'");
 
-})
+    //     console.log( $(this).text() );
+        
+    // });
+
+    $(document).on('click', '.btn', function(){
+    // console.log( $(this).text() );
+
+    var btnName = $(this).text();
 
 
+    const api_key = 'QZDK12gEoyQSqJjGcUf0zhRnzG6Mqgaw';
 
+    const queryURL = "http://api.giphy.com/v1/gifs/search?q=" + btnName + "&api_key="+ api_key +"&limit=20";
+
+    $.ajax({
+        type: 'GET',
+        url: queryURL
+    }).then(function(response){
+
+        console.log(response);
+
+        // console.log(response.data[0].rating);
+
+        // console.log(response.data[0].title);
+
+        // var rating = response.data[0].rating
+        // var images = response.data[0].images.fixed_height
+        var url = response.data[0].images.fixed_height.url
+
+        console.log(response.data[0].images.fixed_height.url);
+        // $(this).html(response.data[0].images.fixed_height.url);
+        // $('#giphy-View').append(url); 
+
+        var imgUrl = $('<img>');
+        imgUrl.attr('src', url);
+        imgUrl.attr('alt', 'giphies image');
+        $('#giphy-View').append(imgUrl);
+
+
+    });
+
+});
+
+});
+
+
+// жишээ яаж 
+
+// $(document).on('click', '.btn', function(){
+//     console.log( $(this) );
+
+// });
