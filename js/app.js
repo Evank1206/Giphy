0442171838a8a4c4114ab264eeaeb0c865424ad1
Jatinokov <Enkhtaivan1206@gmail.com>
2019-11-02 21:13:56 -0700

    styled , displied more image in JS, edited in html

--- js/app.js
+++ js/app.js
@@ -9,11 +9,9 @@ var giphies = ['Mr. Joker', 'Mr.Bean', 'Mr.Sponge Bob'];
         $('#button-View').empty();
         for(var i = 0; i < giphies.length; i++){
                 
-            $('#button-View').append('<button class="btn">' + giphies[i] + '</button>');
+        $('#button-View').append('<button class="btn">' + giphies[i] + '</button>');
         //  console.log(giphies[i]);  
-
         };
-    
     };
     renderButtons();
         
@@ -21,48 +19,45 @@ var giphies = ['Mr. Joker', 'Mr.Bean', 'Mr.Sponge Bob'];
         // This function handles events where one button is clicked
 
     $('#add-Giphy').on('click', function(event){
-        event.preventDefault();
+                                         event.preventDefault();
 
     var giphy = $('#giphy-Input').val();
 
     $('#button-View').append('<button class="btn">' + giphy + '</button>');
         // console.log(giphy);
-
-
     });
 
     renderButtons();
 
     // $('.btn').on('click', function(){
     
-    //     // console.log("i'm mr coffee'");
-
+    //     console.log("i'm mr coffee'");
     //     console.log( $(this).text() );
         
     // });
+    // дээрх жишжээ болон дараах жишээ 2 адил
 
     $(document).on('click', '.btn', function(){
     // console.log( $(this).text() );
 
     var btnName = $(this).text();
 
-
     const api_key = 'QZDK12gEoyQSqJjGcUf0zhRnzG6Mqgaw';
 
-    const queryURL = "http://api.giphy.com/v1/gifs/search?q=" + btnName + "&api_key="+ api_key +"&limit=20";
+    const queryURL = "http://api.giphy.com/v1/gifs/search?q=" + btnName + "&api_key="+ api_key +"&limit=21";
 
     $.ajax({
         type: 'GET',
         url: queryURL
     }).then(function(response){
 
-        console.log(response);
+        // console.log(response);
 
-        for(var i = 0; i < 20; i++){
+        for(var i = 0; i < 3; i++){
 
         var url = response.data[i].images.fixed_height.url
 
-        console.log(response.data[i].images.fixed_height.url);
+        // console.log(response.data[i].images.fixed_height.url);
         // $(this).html(response.data[0].images.fixed_height.url);
         // $('#giphy-View').append(url); 
 
@@ -76,11 +71,3 @@ var giphies = ['Mr. Joker', 'Mr.Bean', 'Mr.Sponge Bob'];
 });
 
 });
-
-
-// жишээ яаж 
-
-// $(document).on('click', '.btn', function(){
-//     console.log( $(this) );
-
-// });
