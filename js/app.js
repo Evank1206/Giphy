605a9daa84f15103e57d05c84ba8327063bedab6
Jatinokov <Enkhtaivan1206@gmail.com>
2020-03-30 17:58:05 -0700

    styling in button-container, giphy-container aslo gave as major same size for ImageUrls

--- js/app.js
+++ js/app.js
@@ -1,73 +1,74 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-var giphies = ['Mr. Joker', 'Mr.Bean', 'Mr.Sponge Bob'];
-    // console.log(giphies);
+    var giphies = [];
 
+    function renderButtons() {
 
-    function renderButtons(){
+        $('#button-container').empty();
+        for (var i = 0; i < giphies.length; i++) {
 
-        $('#button-View').empty();
-        for(var i = 0; i < giphies.length; i++){
-                
-        $('#button-View').append('<button class="btn">' + giphies[i] + '</button>');
-        //  console.log(giphies[i]);  
+            $('#button-container').append('<button class="btn">' + giphies[i] + '</button>');
         };
     };
     renderButtons();
-        
 
-        // This function handles events where one button is clicked
+    // This function handles events where one button is clicked
+    $('#add-btn').on('click', function (event) {
+        event.preventDefault();
 
-    $('#add-Giphy').on('click', function(event){
-                                         event.preventDefault();
+        var giphy = $('#giphy-Input').val();
 
-    var giphy = $('#giphy-Input').val();
-
-    $('#button-View').append('<button class="btn">' + giphy + '</button>');
+        $('#button-container').append('<button class="btn">' + giphy + '</button>');
         // console.log(giphy);
     });
 
-    renderButtons();
-
     // $('.btn').on('click', function(){
-    
     //     console.log("i'm mr coffee'");
     //     console.log( $(this).text() );
-        
     // });
     // дээрх жишжээ болон дараах жишээ 2 адил
 
-    $(document).on('click', '.btn', function(){
-    // console.log( $(this).text() );
+    $(document).on('click', '.btn', function () {
 
-    var btnName = $(this).text();
+        /*grabing user input data */
+        var btnName = $(this).text(); //or .val()
+        // console.log(btnName)
 
-    const api_key = 'QZDK12gEoyQSqJjGcUf0zhRnzG6Mqgaw';
+        const api_key = 'QZDK12gEoyQSqJjGcUf0zhRnzG6Mqgaw';
 
-    const queryURL = "http://api.giphy.com/v1/gifs/search?q=" + btnName + "&api_key="+ api_key +"&limit=21";
+        const queryURL = "http://api.giphy.com/v1/gifs/search?q=" + btnName + "&api_key=" + api_key + "&limit=21";
 
-    $.ajax({
-        type: 'GET',
-        url: queryURL
-    }).then(function(response){
+        $.ajax({
+            type: 'GET',
+            url: queryURL
+        }).then(function (response) {
 
-        // console.log(response);
+            // console.log(response);
 
-        for(var i = 0; i < 3; i++){
+            for (var i = 0; i < 21; i++) {
+                // console.log(response.data[i]);
 
-        var url = response.data[i].images.fixed_height.url
+                var url = response.data[i].images.fixed_height.url
 
-        // console.log(response.data[i].images.fixed_height.url);
-        // $(this).html(response.data[0].images.fixed_height.url);
-        // $('#giphy-View').append(url); 
+                // var stopImageUrl = response.data[i].images.fixed_width_small_still.url
+                // console.log(stopImageUrl);
+                
+                // console.log(response.data[i].images.fixed_height.url);
+                // $(this).html(response.data[0].images.fixed_height.url);
+                // $('#giphy-View').append(url); 
 
-        var imgUrl = $('<img>');
-        imgUrl.attr('src', url);
-        imgUrl.attr('alt', 'giphies image');
-        $('#giphy-View').append(imgUrl);
+                var ImageUrl = $('<img width=200 height=200>');
+                ImageUrl.attr('src', url);
+                ImageUrl.attr('alt', 'class="giphies-image"');
+                $('#giphy-container').append(ImageUrl);
 
-        }
+            }
+            // search for event capturing
+            // $('.giphies image').on('click', function(){
+
+            //     var state = $(this).attr("")
+            // })
+        });
     });
-});
 
 });
